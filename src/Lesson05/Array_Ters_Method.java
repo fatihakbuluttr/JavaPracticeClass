841112fbce1c13e3e0f2ee7c7fe8e2d3e3719218
User <User@DESKTOP-ENT97T7>
2021-03-17 00:04:45 +0200

    17.03.2020

--- src/Lesson05/Array_Ters_Method.java
+++ src/Lesson05/Array_Ters_Method.java
@@ -8,11 +8,16 @@ public class Array_Ters_Method {
         Scanner scan = new Scanner(System.in);
         System.out.println("Kelime gir: ");
         String str = scan.next();
+
         System.out.println(  ters(str)     );
     }
+
     public static String ters (String a) {
+
         char [] arr = a.toCharArray();
+
         String b = "";
+
         for (int i = a.length()-1; i>=0; i--) {
             b += arr[i];
         }
